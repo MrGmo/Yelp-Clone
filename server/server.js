1bd74d2c11240138b44664d7f3672e98103571b2
Guillermo Aguilar <gmo@macbook-air.lan>
2021-04-15 21:02:33 -0500

    app update

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+
+app.get('/getRestaurants', (req, res) => {
+  console.log('get all restaurants')
+});
+
+console.log('anl99e')
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+});
