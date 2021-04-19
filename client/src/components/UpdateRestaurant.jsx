a358fae91935aab7c7e17c455bc99a3c9e2cc49b
Guillermo Aguilar <gaguilar128@gmail.com>
2021-04-19 18:19:58 -0500

    Almost done

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { RestaurantsContext } from '../context/RestaurantsContext'
 import RestaurantFinder from'../apis/RestaurantFinder'
-import { useHistory } from "react-router-dom";
 
 const UpdateRestaurant = (props) => {
   const { id } = useParams();
@@ -29,7 +28,7 @@ const UpdateRestaurant = (props) => {
     const updatedRestaurant = await RestaurantFinder.put(`/${id}`, {
       name,
       location,
-      price_range: priceRange
+      price_range: priceRange,
     });
     history.push('/');
   };
@@ -39,20 +38,20 @@ const UpdateRestaurant = (props) => {
     <form action="">
       <div className="form-group">
         <label htmlFor="name">Name</label>
-        <input value={name} onChange={e => setName(e.target.value)} id="name" className="form-control" type="text"/>
+        <input value={name} onChange={(e) => setName(e.target.value)} id="name" className="form-control" type="text"/>
       </div>
       <div className="form-group">
         <label htmlFor="location">Location</label>
-        <input value={location} onChange={e => setLocation(e.target.value)} id="location" className="form-control" type="text"/>
+        <input value={location} onChange={(e) => setLocation(e.target.value)} id="location" className="form-control" type="text"/>
       </div>
       <div className="form-group">
         <label htmlFor="price_range">Price Range</label>
-        <input value={priceRange} onChange={e => setPriceRange(e.target.value)} id="price_range" className="form-control" type="number"/>
+        <input value={priceRange} onChange={(e) => setPriceRange(e.target.value)} id="price_range" className="form-control" type="number"/>
       </div>
-      <button onClick={handleSubmit} type="submit" className="btn btn-primary">Submit</button>
+      <button type="submit" onClick={handleSubmit} className="btn btn-primary">Submit</button>
     </form>
   </div>
-  )
+  );
 };
 
 export default UpdateRestaurant;
