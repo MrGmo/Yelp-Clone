82904fffd0783ffb6a6b1ad9bbbcd12729965a76
Guillermo Aguilar <gaguilar128@gmail.com>
2021-04-19 00:40:33 -0500

    work in progress

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,6 +1,45 @@
-import React from 'react'
+import React, {useEffect, useContext} from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import { useHistory } from "react-router-dom";
 
-const RestaurantList = () => {
+const RestaurantList = (props) => {
+  const {restaurants, setRestaurants} = useContext(RestaurantsContext);
+
+  let history = useHistory()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get('/');
+        setRestaurants(response.data.data.restaurants);
+      } catch (err) {}
+    };
+    fetchData();
+  },[])
+
+  const handleDelete = async (e, id) => {
+    e.stopPropagation()
+    try {
+     const response = await RestaurantFinder.delete(`/${id}`);
+     setRestaurants(restaurants.filter(restaurant => {
+        return restaurant.id !== id
+      })
+     )
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+  const handleUpdate = (e, id) => {
+    e.stopPropagation()
+    history.push(`/restaurants/${id}/update`)
+  }
+
+  const handleRestaurantSelect = (id) => {
+    history.push(`/restaurants/${id}`);
+  }
+
   return (
     <div className = "list-group">
       <table className="table table-hover table-dark">
@@ -15,7 +54,24 @@ const RestaurantList = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          {restaurants && restaurants.map(restaurant => {
+            return (
+              <tr onClick={() => handleRestaurantSelect(restaurant.id)} key={restaurant.id}>
+                <td>{restaurant.name}</td>
+                <td>{restaurant.location}</td>
+                <td>{"$".repeat(restaurant.price_range)}</td>
+                <td>Reviews</td>
+                <td>
+                 <button onClick={(e) => handleUpdate(e, restaurant.id)} className="btn btn-warning">Update</button>
+                </td>
+                <td>
+                 <button onClick={(e) => handleDelete(e, restaurant.id)} className="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+             );
+            })}
+
+          {/* <tr>
             <td>McDonalds</td>
             <td>New York</td>
             <td>$$</td>
@@ -30,7 +86,7 @@ const RestaurantList = () => {
             <td>Rating</td>
             <td><button className="btn btn-warning">Update</button></td>
             <td><button className="btn btn-danger">Delete</button></td>
-          </tr>
+          </tr> */}
         </tbody>
       </table>
     </div>
--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useContext } from "react"
+import { useParams } from "react-router-dom"
+import { RestaurantsContext } from "../context/RestaurantsContext"
 
 const RestaurantDetailPage = () => {
-  return (
-    <div>
-      Detail Page
-    </div>
-  )
-}
+  const { id } = useParams()
+  const { selectedRestaurant, setSelectedRestaurant } = useContext(RestaurantsContext)
+  return <div>Detail Page</div>
+};
 
 export default RestaurantDetailPage
